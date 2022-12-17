96f28ee5790a6d2f19bed62e16a007d070010a3c
Gustavo Enricco Goncalves Alexandre <gustavoalexandre@employer.com.br>
2022-12-16 22:13:24 -0300

    007 - DetalhesDoPrato

---
+++ AppShopping_/AppShopping_/Services/PratosService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping_.Services
+{
+    class PratosService
+    {
+        internal object getPratos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AppShopping_/AppShopping_/ViewModel/PratosPrincipaisDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AppShopping_.Libraries.Helpers.MVVM;
+using AppShopping_.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppShopping_.ViewModel
+{
+    class PratosPrincipaisDetailViewModel:BaseViewModel
+    {
+
+        public Pratos Pratos { get; set; }
+
+        public PratosPrincipaisDetailViewModel() //ctor vai definir valor p estabelecimento
+        {
+            Pratos = new PratosService().getPratos().First();
+        }
+    }
+}
